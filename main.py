aad43d171efddbe076946881cf35dd70029b6e29
RCRalph <r.chrzanowski.priv@gmail.com>
2021-09-10 21:39:15 +0200

    Added announcement functionality

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import discord
 import datetime
+import json
+
 from decouple import config
+from discord.ext import tasks
 
+dateFormat = "%Y-%m-%d %H:%M"
 
+@tasks.loop(minutes = 0.1)
+async def check_for_announcement():
+    agenda = json.load(open('agenda.json'))
+    messageChannel = client.get_channel(int(config('ANNOUNCEMENT_CHANNEL_ID')))
+    currentDate = datetime.datetime.now().strftime(dateFormat)
+    for i in agenda:
+        agendaDate = datetime.datetime.strptime(i, dateFormat).strftime(dateFormat)
+        if (currentDate == agendaDate):
+            await messageChannel.send(agenda[i])
+
+@check_for_announcement.before_loop
+async def before():
+    print("Starting the bot...")
+    await client.wait_until_ready()
 
 class Client(discord.Client):
-
     async def on_ready(self):
         print(f'[{datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")}] Logged in as {self.user}')
-
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -34,7 +50,7 @@ class Client(discord.Client):
                 embed = discord.Embed(title="Work on you projects!", color=0x7845d1)
             await message.channel.send(embed=embed)
         
-        elif message.content.startwith('!help'):
+        elif message.content.startswith('!help'):
             embed = discord.Embed(title='Available Commands', color=0x000000)
 
     async def on_member_join(self, member):
@@ -43,16 +59,15 @@ class Client(discord.Client):
 
     async def on_raw_reaction_add(self, payload):
         #Add roles after reaction to specific message
-        if str(payload.message_id) != config('MESSAGE_ID'):
+        if payload.message_id != int(config('MESSAGE_ID')):
             return
         if str(payload.emoji) == '❤️':
             print('czerwone')
         elif str(payload.emoji) == '💙':
             print('niebieskie')
         
-
 intents = discord.Intents.all() #TODO: set intents
-
-
 client = Client(intents=intents)
+
+check_for_announcement.start()
 client.run(config('BOT_TOKEN'))
