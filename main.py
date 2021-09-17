6da7a28e26d3ff01555aa150c0628138ebe6c999
RCRalph <rcralph@rcralph.me>
2021-09-18 00:45:50 +0200

    Added workshop agenda, finished announcements and !agenda response

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import discord
 import datetime
 import json
+import aiocron
 
 from decouple import config
-from discord.ext import tasks
 
-dateFormat = "%Y-%m-%d %H:%M"
+dateFormat, color = "%Y-%m-%d %H:%M", 0x7845d1
 
-@tasks.loop(minutes = 1)
+@aiocron.crontab("* * * * *")
 async def check_for_announcement():
-    agenda = json.load(open('agenda.json'))
+    agenda = json.load(open(config("AGENDA_FILE_NAME")))
     messageChannel = client.get_channel(int(config('ANNOUNCEMENT_CHANNEL_ID')))
-    currentDate = datetime.datetime.now().strftime(dateFormat)
-    for i in agenda:
-        agendaDate = datetime.datetime.strptime(i, dateFormat).strftime(dateFormat)
-        if (currentDate == agendaDate):
-            await messageChannel.send(agenda[i])
+    currentDate = datetime.datetime.now()
 
-@check_for_announcement.before_loop
-async def before():
-    print("Starting the bot...")
-    await client.wait_until_ready()
+    for i in agenda:
+        startDate = datetime.datetime.strptime(i["start"], dateFormat)
+        if (currentDate.strftime(dateFormat) == startDate.strftime(dateFormat)):
+            embed = discord.Embed(title="Reminder!", color=color)
+
+            startHour = datetime.datetime.strptime(i["start"], dateFormat).strftime("%H:%M")
+            endHour = datetime.datetime.strptime(i["end"], dateFormat).strftime("%H:%M")
+            embed.add_field(name=i["title"], value=f"{startHour} - {endHour}", inline=False)
+
+            await messageChannel.send(embed=embed)
+            print("Sent announcement message")
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -34,24 +37,34 @@ class Client(discord.Client):
             await message.channel.send('Hello!')
         
         elif message.content.startswith('!agenda'):
-            if datetime.date.today == datetime.date(2021, 9, 25):
-                embed=discord.Embed(title="Hackathon Agenda (25.09)", color=0x7845d1)
-                embed.add_field(name="Kick-off Lecture", value="12:00 - 13:00", inline=False)
-                embed.add_field(name="Matchmaking", value="13:00 - 14:00", inline=False)
-                embed.add_field(name="Quantum Games - Piotr Biskupi", value="14:00 - 15:00", inline=False)
-                embed.add_field(name="Quantum Computing - Piotr Gawron", value="15:00 - 16:00", inline=False)
-                embed.add_field(name="JavaScript Programming Language- Piotr Migdał", value="17:00 - 19:00", inline=False)
-            elif datetime.date.today == datetime.date(2021, 9, 26):
-                embed = discord.Embed(title="Hackathon Agenda (26.09)", color=0x7845d1)
-                embed.add_field(name='Playablity - Artur Roszczyk (Orbital Knight)', value='12:00 - 13:00', inline=False)
-                embed.add_field(name='\"Linear algebra is actually a fun videogame: Quantum Odyssey\"', value='14:00 - 15:00', inline=False)
-                embed.add_field(name='User Experience - Klem Jankiewicz', value='17:00 -18:00', inline=False)
+            agenda = json.load(open(config("AGENDA_FILE_NAME")))
+            currentDate = datetime.datetime.today()
+
+            todaysAgenda = []
+            for i in agenda:
+                startDate = datetime.datetime.strptime(i["start"], dateFormat)
+                if (startDate.strftime("%Y-%m-%d") == currentDate.strftime("%Y-%m-%d")):
+                    todaysAgenda.append(i)
+            
+            if (todaysAgenda):
+                embed = discord.Embed(title="Agenda ({})".format(currentDate.strftime("%Y-%m-%d")), color=color)
+                for i in todaysAgenda:
+                    startHour = datetime.datetime.strptime(i["start"], dateFormat).strftime("%H:%M")
+                    endHour = datetime.datetime.strptime(i["end"], dateFormat).strftime("%H:%M")
+                    embed.add_field(name=i["title"], value=f"{startHour} - {endHour}", inline=False)
             else:
-                embed = discord.Embed(title="Work on you projects!", color=0x7845d1)
+                embed = discord.Embed(title="Agenda doesn't exist for the current day!", color=color)
+
             await message.channel.send(embed=embed)
         
         elif message.content.startswith('!help'):
-            embed = discord.Embed(title='Available Commands', color=0x000000)
+            embed = discord.Embed(title='Available Commands', color=color)
+
+            embed.add_field(name="!hello", value="Welcomes the user.", inline=False)
+            embed.add_field(name="!agenda", value="Shows agenda for the current day", inline=False)
+            embed.add_field(name="!help", value="Shows available commands", inline=False)
+
+            await message.channel.send(embed=embed)
 
     async def on_member_join(self, member):
         guest = discord.utils.get(member.guild.roles, name="Guest")
@@ -65,9 +78,10 @@ class Client(discord.Client):
             print('czerwone')
         elif str(payload.emoji) == '💙':
             print('niebieskie')
-        
-intents = discord.Intents.all() #TODO: set intents
-client = Client(intents=intents)
 
-check_for_announcement.start()
+# Start execution
+print("Starting the bot...")
+intents = discord.Intents.all() #TODO: set intents
+
+client = Client(intents=intents)
 client.run(config('BOT_TOKEN'))
