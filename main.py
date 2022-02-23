71abf2534ecd59fa332307d696eea86b59e6efba
RCRalph <rcralph@rcralph.me>
2022-02-23 15:34:24 +0100

    Added agenda-full command, changed times to GMT and added new schedule

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def check_for_announcement():
 
     for j in agenda:
         messageChannel = client.get_channel(j["channel_id"])
-        currentDate = datetime.datetime.now()
+        currentDate = datetime.datetime.now(datetime.timezone.utc)
 
         for i in j["schedule"]:
             startDate = datetime.datetime.strptime(i["start"], dateFormat)
@@ -22,7 +22,7 @@ async def check_for_announcement():
 
                 startHour = datetime.datetime.strptime(i["start"], dateFormat).strftime("%H:%M")
                 endHour = datetime.datetime.strptime(i["end"], dateFormat).strftime("%H:%M")
-                embed.add_field(name=i["title"], value=f"{startHour} - {endHour}", inline=False)
+                embed.add_field(name=i["title"], value=f"{startHour} - {endHour} GMT", inline=False)
 
                 await messageChannel.send(embed=embed)
                 print(f"Sent announcement message: {i['title']}")
@@ -37,6 +37,22 @@ class Client(discord.Client):
 
         elif message.content.startswith('!hello'):
             await message.channel.send(f'Hello {message.author.mention}!')
+
+        elif message.content.startswith('!agenda-full'):
+            currentDate = datetime.datetime.today()
+            agenda = json.load(open(config("SCHEDULES_FILE_NAME")))
+            serverID = message.guild.id
+
+            for j in agenda:
+                if (j["server_id"] == serverID):
+                    embed = discord.Embed(title="Full agenda".format(currentDate.strftime("%Y-%m-%d")), color=color)
+                    for i in j["schedule"]:
+                        startHour = datetime.datetime.strptime(i["start"], dateFormat).strftime("%H:%M")
+                        endHour = datetime.datetime.strptime(i["end"], dateFormat).strftime("%H:%M")
+                        embed.add_field(name=i["title"], value=f"{startHour} - {endHour} GMT", inline=False)
+
+                await message.channel.send(embed=embed)
+                break
         
         elif message.content.startswith('!agenda'):
             agenda = json.load(open(config("SCHEDULES_FILE_NAME")))
@@ -57,12 +73,12 @@ class Client(discord.Client):
                         for i in todaysAgenda:
                             startHour = datetime.datetime.strptime(i["start"], dateFormat).strftime("%H:%M")
                             endHour = datetime.datetime.strptime(i["end"], dateFormat).strftime("%H:%M")
-                            embed.add_field(name=i["title"], value=f"{startHour} - {endHour}", inline=False)
+                            embed.add_field(name=i["title"], value=f"{startHour} - {endHour} GMT", inline=False)
                     else:
                         embed = discord.Embed(title="Today's agenda is empty!", color=color)
 
                     await message.channel.send(embed=embed)
-                    break                    
+                    break
         
         elif message.content.startswith('!help'):
             embed = discord.Embed(title='Available Commands', color=color)
@@ -75,7 +91,7 @@ class Client(discord.Client):
 
 # Start execution
 print("Starting the bot...")
-intents = discord.Intents.all() #TODO: set intents
+intents = discord.Intents.all()
 
 client = Client(intents=intents)
 client.run(config('BOT_TOKEN'))
